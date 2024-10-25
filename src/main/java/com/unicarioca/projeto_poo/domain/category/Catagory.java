55b9b5eb354f057071a99219d9f8d228e12c3252
Felihum <121069824+Felihum@users.noreply.github.com>
2024-10-24 23:19:08 -0300

    Category created.

---
+++ src/main/java/com/unicarioca/projeto_poo/domain/category/Catagory.java
@@ -0,0 +1,29 @@
+package com.unicarioca.projeto_poo.domain.category;
+
+import com.unicarioca.projeto_poo.domain.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Catagory {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
