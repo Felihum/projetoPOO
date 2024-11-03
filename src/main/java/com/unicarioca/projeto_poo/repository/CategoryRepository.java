96488ca172fa9a085839de5cb69c5de22c63c9f9
Cristhian Anthony <menzakinew@gmail.com>
2024-11-03 19:53:59 -0300

    Update no Repositório Categoria
    Prometo ser a ultima atualização por hoje

--- src/main/java/com/unicarioca/projeto_poo/repository/CategoryRepository.java
+++ src/main/java/com/unicarioca/projeto_poo/repository/CategoryRepository.java
@@ -1,11 +1,12 @@
 package com.unicarioca.projeto_poo.repository;
 
-import com.unicarioca.projeto_poo.domain.card.Card;
+import com.unicarioca.projeto_poo.domain.category.ProductCategory;
+import com.unicarioca.projeto_poo.domain.category.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends  JpaRepository<Card, UUID> {
+public interface CategoryRepository extends  JpaRepository<ProductCategory, UUID> {
 }
