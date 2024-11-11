0fbc40bbf52993c86c228d1cc0cc73bd3e455e31
Felihum <121069824+Felihum@users.noreply.github.com>
2024-11-11 19:01:17 -0300

    Migrations adjustment.

--- src/main/resources/db/migration/V3__create-product-table.sql
+++ src/main/resources/db/migration/V3__create-product-table.sql
@@ -4,10 +4,11 @@ CREATE TABLE products (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    discount DECIMAL(10, 2),
+    discount INTEGER,
     final_price DECIMAL(10, 2) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    id_category UUID NOT NULL,
     storage_quantity INTEGER NOT NULL,
     description TEXT,
-    img_url TEXT NOT NULL
+    img_url TEXT NOT NULL,
+    FOREIGN KEY (id_category) REFERENCES categories(id) ON DELETE CASCADE
 );
