e7917ab53ea65e1bfd1d8b1599d5b9cc89adb213
Felihum <121069824+Felihum@users.noreply.github.com>
2024-11-11 21:52:48 -0300

    Some Adjustments.

--- src/main/java/com/unicarioca/projeto_poo/repository/CardRepository.java
+++ src/main/java/com/unicarioca/projeto_poo/repository/CardRepository.java
@@ -1,11 +1,17 @@
 package com.unicarioca.projeto_poo.repository;
 
 import com.unicarioca.projeto_poo.domain.card.Card;
+import com.unicarioca.projeto_poo.domain.category.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, UUID> {
+
+    @Query("SELECT c FROM Card c WHERE c.card_number LIKE %:card_number%")
+    Card findCardByCardNumber(@Param("card_number") String card_number);
 }
